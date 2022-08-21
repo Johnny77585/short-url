447e836da02223ef705211da161f51bb8e2e4f38
Johnny77585 <jkuo2188@gmail.com>
2022-08-21 14:40:24 +0800

    feat: add mongoose&handlebars

--- app.js
+++ app.js
@@ -2,10 +2,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars')
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
 
+
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 // routes setting
+
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.render('index')
 })
 
 // start and listen on the Express server
